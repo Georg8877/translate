055b470ce915f3b821d1327c43f80e860e9030ac
Georg8877 <f-techno@yandex.ru>
2020-02-19 00:38:34 +0300

    translate

---
+++ src/components/LanguageSelector.js
@@ -0,0 +1,32 @@
+import React from "react";
+import LanguageContext from "../context/LanguageContext";
+//import Styles from "./Styles.css";
+
+class LanuageSelector extends React.Component {
+  static contextType = LanguageContext;
+
+  render() {
+    console.log(this.context);
+    return (
+      <div className="container">
+        Select a language:
+        <div>
+          <div className="flagSize">
+            <i
+              className="flag us"
+              onClick={() => this.context.onLanguageChange("english")}
+            />
+          </div>
+          <div className="flagSize">
+            <i
+              className="flag ru"
+              onClick={() => this.context.onLanguageChange("russian")}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default LanuageSelector;
